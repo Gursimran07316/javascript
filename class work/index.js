5c34a065e4dc1d13c22f9ce54b40ce729c0ecb6d
mubashir-dot <mmubimemon@gmail.com>
2020-02-13 09:26:08 +0500

    Js in practice

---
+++ class work/index.js
@@ -0,0 +1,8 @@
+var date = new Date()
+var days = ['sun', 'mon', 'tue', 'wed', 'thur', 'fri', 'sat']
+var dayIndex = date.getDay()
+console.log(days[dayIndex])
+
+var date = new Date()
+var time = date.getHours()
+console.log(time)
