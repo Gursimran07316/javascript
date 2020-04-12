51f8899cb25e58d2e2d442bcfe889b12029ad294
mubashir-dot <mmubimemon@gmail.com>
2020-04-12 14:49:45 +0500

    Frondend mentor challenges

---
+++ sign up form/index.js
@@ -0,0 +1,9 @@
+const form = document.getElementById('form');
+const first = document.getElementById('firstName');
+const last = document.getElementById('lastName');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+})
